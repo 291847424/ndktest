4543a842b256485e86eb1173dd0c19c622b91535
Todd <todd@yeahka.com>
2021-03-08 21:21:08 +0800

    初提交

---
+++ app/src/main/cpp/ArrayUtil.cpp
@@ -0,0 +1,59 @@
+//
+// Created by 29184 on 2021/3/7.
+//
+
+#include <stdlib.h>
+#include <android/log.h>
+#include <iostream>
+#include <assert.h>
+#include <time.h>
+
+#define TAG "TAG"
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+
+namespace ArrayUtil {
+
+    int *create_random_array(int len, int low, int high) {
+        int *arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = rand() % (high - low) + low;
+        }
+        return arr;
+    }
+
+    int *create_nearly_ordered_array(int len, int swapNums) {
+        int *arr = new int[len];
+
+        for (int i = 0; i < len; ++i) {
+            arr[i] = i;
+        }
+
+        for (int i = 0; i < swapNums; ++i) {
+            int randomX = rand() % (len - 1);
+            int randomY = rand() % (len - 1);
+            std::swap(arr[randomX], arr[randomY]);
+        }
+        return arr;
+    }
+
+    int *copy_random_array(int *arr, int len) {
+        int *copy_arr = new int[len];
+        memcpy(copy_arr, arr, sizeof(int) * len);
+        return arr;
+    }
+
+    void sort_array(char *sortName, void (*sort)(int *, int), int *arr, int len) {
+        size_t start_time = clock();
+        sort(arr, len);
+        size_t end_time = clock();
+
+        double time = static_cast<double>(end_time - start_time) / CLOCKS_PER_SEC;
+        LOGE("%s的执行时间：%lf", sortName, time);
+
+        for (int i = 0; i < len - 1; i++) {
+            assert(arr[i] <= arr[i + 1]);
+        }
+    }
+
+}
+
---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,75 @@
+#include <jni.h>
+#include <string>
+#include <android/log.h>
+#include "ArrayUtil.cpp"
+
+#define TAG "TAG"
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+
+using namespace std;
+
+void print_array(int arr[], int len) {
+    for (int i = 0; i < len; i++) {
+        LOGE("%d", arr[i]);
+    }
+}
+
+void bubbleSort(int arr[], int len) {
+
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = 0; j < len - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void optimizeBubbleSort(int arr[], int len) {
+
+    int n = len;
+    do {
+        int newn = 0;
+        for (int i = 1; i < n; i++) {
+            if (arr[i - 1] > arr[i]) {
+                swap(arr[i - 1], arr[i]);
+                newn = i;
+            }
+        }
+        n = newn;
+    } while (n > 0);
+}
+
+void selectSort(int arr[], int len) {
+
+    for (int i = 0; i < len - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < len; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+
+}
+
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_ndktest_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+
+    int len = 200;
+    int *arr = ArrayUtil::create_random_array(len, 100, 200);
+    int *arr2 = ArrayUtil::copy_random_array(arr, len);
+
+    ArrayUtil::sort_array("selectSort", selectSort, arr, len);
+    ArrayUtil::sort_array("bubbleSort", bubbleSort, arr2, len);
+
+    delete[] (arr);
+    delete[] (arr2);
+
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
